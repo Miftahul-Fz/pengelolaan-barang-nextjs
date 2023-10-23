f562596b6d28d838eb4818fa3eabb60b952ed89b
3202102012 <miftahulwilna@gmail.com>
2023-10-23 14:20:25 +0700

    feat: implementasi fungsi delete data barang

--- pages/barang/index.tsx
+++ pages/barang/index.tsx
@@ -1,4 +1,7 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
+import Link from "next/link";
+import editBarang from "./edit/[id]";
+import { useRouter } from 'next/router';
 
 type Barang = {
     id: number;
@@ -7,6 +10,8 @@ type Barang = {
 }
 
 export default function indexBarang() {
+    const Router = useRouter();
+    const { id } = Router.query;
     const [barangs, setBarangs] = useState<Barang[]>([]);
 
     const fetchBarang = async () => {
@@ -33,14 +38,38 @@ export default function indexBarang() {
         }
     }, [setBarangs])
 
+    const handleTambah = () => {
+        window.location.href= "/barang/store"
+    }
+    const handleHapus = async (id: number) => {
+        try {
+            const response = await fetch(`http://localhost:3000/api/barang/${id}`,{
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json',
+                },
+                body: JSON.stringify(editBarang)
+            })
+            if (response.ok) {
+                setBarangs((prevBarangs) => prevBarangs.filter((barang) => barang.id !== id));
+            } else {
+                console.error('Gagal menghapus item');
+              }
+        } catch (error) {
+            console.error('terjasi keasalahan', Error)
+        }
+    }
+
     return (
-        <div className="main-content mt-5">
-            <div>
+        <div>
+            <div className="main-content mt-5">
                 <h1 className="text-primary text-center">Data Barang Gudang</h1>
             </div>
             <div className="container">
                 <div className="row justify-content-center">
                     <div className="col-md-10">
+                        <div><button onClick={handleTambah}> tambah data </button></div>
                         <div className="card">
                             <div className="card-body">
                                 <table className="table table-borderedx" >
@@ -49,14 +78,21 @@ export default function indexBarang() {
                                             <th>No</th>
                                             <th>Nama</th>
                                             <th>Stok</th>
+                                            <th>Tags</th>
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        {barangs.map((item, index) => (
-                                        <tr key={item.id}>
+                                        {barangs.map((barang, index) => (
+                                        <tr key={barang.id}>
                                             <td> {index + 1} </td>
-                                            <td> {item.nama} </td>
-                                            <td> {item.stok} </td>
+                                            <td> {barang.nama} </td>
+                                            <td> {barang.stok} </td>
+                                            <td>
+                                                <Link href={`/barang/edit/${barang.id}`}>
+                                                <button>Edit</button>
+                                                </Link>
+                                                <button onClick={() => handleHapus(barang.id)}>Hapus</button>
+                                            </td>
                                         </tr>
                                         ))}
                                     </tbody>
